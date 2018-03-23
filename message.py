e9f9402e91526be5d47c437d8956e575e0d6c348
John Crocker <jcrocker@zenoss.com>
2018-03-23 11:45:10 -0500

    Concurrent version of message handling

--- message.py
+++ message.py
@@ -7,29 +7,43 @@ Context = namedtuple('Context', ('RTS', 'RRTS', 'Transitions'))
 
 MAXINT=9
 
-def onMessage(n,t,x, ctx):
+def onMessage(n,t,x,ctx):
+    f = onMessageConcurrent(n,t,x,ctx)
+    for _ in f:
+        pass
+
+def onMessageConcurrent(n,t,x, ctx):
     RTS, RRTS, Transitions = ctx.RTS, ctx.RRTS, ctx.Transitions
+    sequence = 0
     RTS.insert((n,-t), x)
+    yield 0, sequence, -t ; sequence += 1
+
     RRTS.insert((n,t), 0)
+    yield 1, sequence, t ; sequence += 1
 
     try:
         prev = RTS.scan((n,-t), (n+'\0', 0))[1]
+        yield 2, sequence, -prev[0].time ; sequence += 1
     except IndexError:
         u,y = 0, []
         RTS.insert((n,u), y)
+        yield 3, sequence, None ; sequence += 1
     else:
         u,y = prev[0].time, prev[1]
 
     try:
         following = RRTS.scan((n,t), (n+'\0', 0))[1]
+        yield 4, sequence, following[0].time; sequence += 1
     except IndexError:
         s = MAXINT
         RTS.insert((n,-s), [])
+        yield 5, sequence, None; sequence += 1
         RRTS.insert((n,s), 0)
+        yield 6, sequence, None ; sequence += 1
     else:
         s = following[0].time
     s = -s
-    
+
     transitions = []
     while True:
         # Insert transitions to adjacent messages 
@@ -39,16 +53,20 @@ def onMessage(n,t,x, ctx):
             break
         for i in to_insert:
             Transitions.insert(i,None)
+            yield 7, sequence, i[1:] ; sequence += 1
 
         # Get relevant messages and transitions
         messages = RTS.scan((n,s),(n,u), True)
+        yield 8, sequence, (s,u,[i[0].time for i in messages]) ; sequence += 1
         transitions = [i[0] for i in Transitions.scan((n,-t,-MAXINT),(n,u,MAXINT))]
+        yield 9, sequence, (-t,u, [i[1:] for i in transitions]); sequence += 1
 
         # Delete messages
         q = [i for i in transitions if i.precedent == u]
         if q[-1] == (n,u,-t):
             for i in q[:-1]:
                 Transitions.delete(i)
+                yield 10, sequence, i[1:] ; sequence += 1
 
         # Find adjacent messages
         for idx, val in enumerate(messages):
@@ -58,39 +76,4 @@ def onMessage(n,t,x, ctx):
                 break
         else:
             assert False
-
-
-
-
-
-def onMessageConcurrent(n,t,x, ctx):
-    RTS, Transitions = ctx.RTS, ctx.Transitions
-    RTS.insert((n,-t), x)
-    yield #1
-
-    try:
-        prev = RTS.scan((n,-t), (n+'\0', 0))[1]
-    except IndexError:
-        u,y = 0, []
-    else:
-        u,y = prev[0].time, prev[1] 
-    yield #2
-
-    Transitions.insert((n,u,-t), None)
-    yield #3
-
-    transitions = [i[0] for i in Transitions.prefix((n,u))]
-    s = min(i.subsequent for i in transitions)
-    yield #4
-
-    messages = RTS.scan((n,s), (n,u), True)
-    if (u == 0):
-        messages.append((RTSKey('n',0),[]))
-    expected = [TransitionsKey(n,i[0].time,j[0].time) for i,j in zip(messages[1:], messages[:-1])]
-    yield #5
-
-    for t in set(transitions) - set(expected):
-        Transitions.delete(t)
-    for t in set(expected) - set(transitions):
-        Transitions.insert(t, None)
-    yield #6
+    yield 11, sequence, None
--- test_message.py
+++ test_message.py
@@ -1,8 +1,9 @@
 import unittest
-from message import onMessage, onMessageConcurrent, Context, MAXINT
+from message import onMessage, onMessageConcurrent, Context, MAXINT as MAX
 from table import Table, RTSKey, TransitionsKey, RRTSKey
 
 rts_exp = lambda x,y: [(RTSKey(x,-i),[]) for i in y]
+rrts_exp = lambda x,y: [(RRTSKey(x,i),0) for i in y]
 tr_exp = lambda x,y: [(TransitionsKey(x,-i,-j),None) for i,j in y]
 
 
@@ -17,8 +18,8 @@ class TestMessage(unittest.TestCase):
         onMessage('n', 1, [], self.ctx)
         rts = self.ctx.RTS.prefix('n')
         tr = self.ctx.Transitions.prefix('n')
-        self.assertEqual(rts, rts_exp('n', (MAXINT,1,0)))
-        self.assertEqual(tr, tr_exp('n',((1,9),(0,1))))
+        self.assertEqual(rts, rts_exp('n', (MAX,1,0)))
+        self.assertEqual(tr, tr_exp('n',((1,MAX),(0,1))))
 
     def test_sequential(self):
         onMessage('n', 1, [], self.ctx)
@@ -26,8 +27,8 @@ class TestMessage(unittest.TestCase):
         onMessage('n', 3, [], self.ctx)
         rts = self.ctx.RTS.prefix('n')
         tr = self.ctx.Transitions.prefix('n')
-        self.assertEqual(rts, rts_exp('n',(MAXINT,3,2,1,0)))
-        self.assertEqual(tr, tr_exp('n', ((3,9),(2,3),(1,2),(0,1))))
+        self.assertEqual(rts, rts_exp('n',(MAX,3,2,1,0)))
+        self.assertEqual(tr, tr_exp('n', ((3,MAX),(2,3),(1,2),(0,1))))
 
     def test_outOfOrder(self):
         onMessage('n', 1, [], self.ctx)
@@ -35,48 +36,68 @@ class TestMessage(unittest.TestCase):
         onMessage('n', 2, [], self.ctx)
         rts = self.ctx.RTS.prefix('n')
         tr = self.ctx.Transitions.prefix('n')
-        self.assertEqual(rts, rts_exp('n',(MAXINT,3,2,1,0)))
-        self.assertEqual(tr, tr_exp('n', ((3,9),(2,3),(1,2),(0,1))))
+        self.assertEqual(rts, rts_exp('n',(MAX,3,2,1,0)))
+        self.assertEqual(tr, tr_exp('n', ((3,MAX),(2,3),(1,2),(0,1))))
 
-"""
     def test_concurrent(self):
         rts = lambda: self.ctx.RTS.prefix('n')
+        rrts = lambda: self.ctx.RRTS.prefix('n')
         trs = lambda: self.ctx.Transitions.prefix('n')
         onMessage('n', 1, [], self.ctx)
         onMessage('n', 4, [], self.ctx)
         t2 = onMessageConcurrent('n', 2, [], self.ctx)
         t3 = onMessageConcurrent('n', 3, [], self.ctx)
-        self.assertEqual(rts(), rts_exp('n',(4,1)))
-        self.assertEqual(trs(), tr_exp('n', ((1,4),(0,1))))
-        # 1 (Write to RTS)
-        t2.next()
-        t3.next()
-        self.assertEqual(rts(), rts_exp('n', (4,3,2,1)))
-        self.assertEqual(trs(), tr_exp('n', ((1,4),(0,1))))
-        # 2 (Read previous state from RTS)
-        t2.next()
-        t3.next()
-        # 3 (Write to Transitions)
-        t2.next()
-        t3.next()
-        self.assertEqual(rts(), rts_exp('n', (4,3,2,1)))
-        self.assertEqual(trs(), tr_exp('n', ((2,3),(1,4),(1,2),(0,1))))
-        # 4 (Read transitions)
-        t2.next()
-        t3.next()
-        # 5 (Read messages)
-        t2.next()
-        t3.next()
-        # 6 (Fix up)
-        self.assertEqual(rts(), rts_exp('n', (4,3,2,1)))
-        self.assertEqual(trs(), tr_exp('n', ((2,3),(1,4),(1,2), (0,1))))
-        t2.next()
-        t3.next()
-        self.assertEqual(rts(), rts_exp('n', (4,3,2,1)))
-        self.assertEqual(trs(), tr_exp('n', ((3,4),(2,3),(1,2),(0,1))))
+        self.assertEqual(rts(), rts_exp('n',(MAX,4,1,0)))
+        self.assertEqual(rrts(), rrts_exp('n',(1,4,MAX)))
+        self.assertEqual(trs(), tr_exp('n', ((4,MAX),(1,4),(0,1))))
+        # 0:t2 (Write to RTS)
+        self.assertEqual(t2.next(), (0,0,-2))
+        self.assertEqual(rts(), rts_exp('n', (MAX,4,2,1,0)))
+        # 0:t3 (Write to RTS)
+        self.assertEqual(t3.next(), (0,0,-3))
+        self.assertEqual(rts(), rts_exp('n', (MAX,4,3,2,1,0)))
+        # 1:t2 (Write to RRTS)
+        self.assertEqual(t2.next(), (1,1,2))
+        self.assertEqual(rrts(), rrts_exp('n',(1,2,4,MAX)))
+        # 1:t3 (Write to RRTS)
+        self.assertEqual(t3.next(), (1,1,3))
+        self.assertEqual(rrts(), rrts_exp('n',(1,2,3,4,MAX)))
+        # 2 (Scan RTS for precedent)
+        self.assertEqual(t2.next(), (2,2,1))
+        self.assertEqual(t3.next(), (2,2,2))
+        # 4 (Scan RRTS for subsequent)
+        self.assertEqual(t2.next(), (4,3,3))
+        self.assertEqual(t3.next(), (4,3,4))
+        # 7:t2 (Insert transition - incoming)
+        self.assertEqual(t2.next(), (7,4,(-1,-2)))
+        self.assertEqual(trs(), tr_exp('n', ((4,MAX),(1,4),(1,2),(0,1))))
+        # 7:t3 (Insert transition - incoming)
+        self.assertEqual(t3.next(), (7,4,(-2,-3)))
+        self.assertEqual(trs(), tr_exp('n', ((4,MAX),(2,3),(1,4),(1,2),(0,1))))
+        # 7:t2 (Insert transition - outgoing)
+        self.assertEqual(t2.next(), (7,5,(-2,-3)))
+        self.assertEqual(trs(), tr_exp('n', ((4,MAX),(2,3),(1,4),(1,2),(0,1))))
+        # 7:t3 (Insert transition - outgoing)
+        self.assertEqual(t3.next(), (7,5,(-3,-4)))
+        self.assertEqual(trs(), tr_exp('n', ((4,MAX),(3,4),(2,3),(1,4),(1,2),(0,1))))
+        # 8 (Scan RTS)
+        self.assertEqual(t2.next(), (8,6,(-3,-1,[-3,-2,-1])))
+        self.assertEqual(t3.next(), (8,6,(-4,-2,[-4,-3,-2])))
+        # 9 (Scan Transition)
+        self.assertEqual(t2.next(), (9,7,(-2,-1,[(-2,-3),(-1,-4),(-1,-2)])))
+        self.assertEqual(t3.next(), (9,7,(-3,-2,[(-3,-4),(-2,-3)])))
+        # 10:t2 (Delete Transition)
+        self.assertEqual(t2.next(), (10,8,(-1,-4)))
+        self.assertEqual(trs(), tr_exp('n', ((4,MAX),(3,4),(2,3),(1,2),(0,1))))
+        # 11 (Done)
+        self.assertEqual(t2.next(), (11,9,None))
+        self.assertEqual(t3.next(), (11,8,None))
+        self.assertEqual(rts(), rts_exp('n', (MAX,4,3,2,1,0)))
+        self.assertEqual(rrts(), rrts_exp('n',(1,2,3,4,MAX)))
+        self.assertEqual(trs(), tr_exp('n', ((4,MAX),(3,4),(2,3),(1,2),(0,1))))
         with self.assertRaises(StopIteration): t2.next()
         with self.assertRaises(StopIteration): t3.next()
-"""
+
 """
 class MyTestsMeta(type):
     def __new__(cls, name, bases, attrs):
