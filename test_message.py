81fdf70107ec9530e44f874a3f86c08011c9c8ba
John Crocker <jcrocker@zenoss.com>
2018-03-26 08:50:57 -0500

    Enable the "test all sequences" test
    Note: all tests do not pass at this commit

--- test_message.py
+++ test_message.py
@@ -98,53 +98,68 @@ class TestMessage(unittest.TestCase):
         with self.assertRaises(StopIteration): t2.next()
         with self.assertRaises(StopIteration): t3.next()
 
-"""
 class MyTestsMeta(type):
     def __new__(cls, name, bases, attrs):
-        for a in range(0,7):
-            for b in range(a,7):
-                for c in range(b,7):
-                    for d in range(c,7):
-                        for e in range(d,7):
-                            for f in range(e,7):
-                                name,fn = cls.gen(a,b,c,d,e,f)
-                                attrs[name] = fn
+        threads =2 
+        iterations = 12
+        slots = threads*iterations
+        for i in range(threads**slots):
+            break
+            c=[]
+            cnt=[0]*threads
+            for j in range(slots):
+                t = (i/(threads**j))%threads
+                cnt[t] += 1
+                if cnt[t] > iterations:
+                    break
+                c.append(t)
+            else:
+                name = "test_{}-{}".format(threads,''.join(str(i) for i in c))
+                attrs[name] = cls.gen(threads, c)
+        t='111111100000000001111100'
+        name = "test_{}-{}".format(threads,''.join(str(i) for i in t))
+        attrs[name] = cls.gen(threads, [int(i) for i in t])
+
+
 
         return super(MyTestsMeta, cls).__new__(cls, name, bases, attrs)
 
     @classmethod
-    def gen(cls, *args):
-        # Return a testcase that tests (a,b,c,d,e,f).
-        assert len(args) == 6 and min(args) >= 0 and max(args) <= 6
-        name = ''.join(str(i) for i in args)
+    def gen(cls, n, order):
         def fn(self):
-            self.assertEqual(self.doit(args), None)
-        return 'test_'+name, fn
+            self.doit(n, order)
+        return fn
 
 class MyTests(unittest.TestCase):
     __metaclass__ = MyTestsMeta
     def setUp(self):
         RTS=Table(RTSKey)
+        RRTS=Table(RRTSKey)
         Transitions=Table(TransitionsKey)
-        self.ctx = Context(RTS, Transitions)
+        self.ctx = Context(RTS, RRTS, Transitions)
 
-    def doit(self, order):
+    def doit(self, threadCnt, order):
         onMessage('n', 1, [], self.ctx)
-        onMessage('n', 4, [], self.ctx)
-        t2 = onMessageConcurrent('n', 2, [], self.ctx)
-        t3 = onMessageConcurrent('n', 3, [], self.ctx)
+        threads = [onMessageConcurrent('n', 2+i, [], self.ctx) for i in range(threadCnt)]
 
-        for i in range(7):
-            for j in range(6):
-                if order[j]==i:
-                    t3.next()
-            if i != 6:
-                t2.next()
+        for i in order:
+            if threads[i]:
+                try: print i,threads[i].next()
+                except StopIteration: threads[i] = None
 
-        with self.assertRaises(StopIteration): t2.next()
-        with self.assertRaises(StopIteration): t3.next()
+        running = [i for i in threads if i]
+        self.assertTrue(len(running)<2)
+
+        for i in running:
+            while True:
+                try: i.next()
+                except StopIteration: break
+
+        msgs = [MAX] + range(threadCnt+1,-1,-1)
         self.assertEqual(self.ctx.RTS.prefix('n'), 
-                rts_exp('n', (4,3,2,1)))
+                rts_exp('n', msgs))
+        self.assertEqual(self.ctx.RRTS.prefix('n'), 
+                rrts_exp('n', msgs[-2::-1]))
+        trs =[(i,j) for i,j in zip(msgs[1:],msgs[:-1])] 
         self.assertEqual(self.ctx.Transitions.prefix('n'), 
-                tr_exp('n', ((3,4),(2,3),(1,2),(0,1))))
-"""
+                tr_exp('n', trs))
